c739931e989de7189b3db276e9a25d16bfc02d46
FilipMachinia <f.machinia@gmail.com>
2021-06-18 20:34:21 +0100

    fixed getAllFiles

--- backend/routes/getAllFiles.js
+++ backend/routes/getAllFiles.js
@@ -1,20 +1,13 @@
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
 
-router.post('/', function (req, res, next) {
+router.get('/getAllFiles', function (req, res, next) {
 
-    fs.readFile('routes/allFiles.json', 'utf8', function (err, data) {
-        let json = JSON.parse(data);
-        json.push(req.body);
-
-        fs.writeFile('routes/allFiles.json', JSON.stringify(json), (err) => {
-            if (err) throw err;
-            res.status(200).send({message: 'Data written to file'})
-        });
-    })
-
-
+  fs.readFile('routes/allFiles.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    res.status(200).send(data)
+  })
 });
 
 module.exports = router;
